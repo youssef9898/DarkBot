eb60bd6e47870429c91358d784cb2e44acb90ab0
BestCoderz <utk.bkkg@gmail.com>
2018-10-16 17:28:52 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -79,8 +79,6 @@ async def access(ctx, member: discord.Member):
         await client.add_roles(member, role)
         embed=discord.Embed(title="User Got Access!", description="**{0}** got access from **{1}**!".format(member, ctx.message.author), color=0xff00f6)
         await client.say(embed=embed)
-	await asyncio.sleep(2700)
-	await client.remove_roles(member, role)
 	
      
 @client.command(pass_context = True)
